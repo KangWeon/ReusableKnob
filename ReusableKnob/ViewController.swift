9286d58b435f01736bec73e226d5f04671ee04fc
KangWeon <kangweon.jeong@gmail.com>
2018-08-20 20:39:32 +0900

    How To Make a Custom Control Tutorial: A Reusable Knob

--- ReusableKnob/ViewController.swift
+++ ReusableKnob/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var valueLabel: UILabel!
+    @IBOutlet var valueSlider: UISlider!
+    @IBOutlet var animateSwitch: UISwitch!
+    @IBOutlet var knob: Knob!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        knob.lineWidth = 4
+        knob.pointerLength = 12
+        knob.setValue(valueSlider.value)
+        updateLabel()
+        knob.addTarget(self, action: #selector(ViewController.handleValueChanged(_:)), for: .valueChanged)
+
+    }
+    
+    @IBAction func handleValueChanged(_ sender: Any) {
+        if sender is UISlider {
+            knob.setValue(valueSlider.value)
+        } else {
+            valueSlider.value = knob.value
+        }
+        updateLabel()
+    }
+    
+    @IBAction func handleRandomButtonPressed(_ sender: Any) {
+        let randomValue = Float.random(in: 0..<101) / 100.0
+        knob.setValue(randomValue, animated: animateSwitch.isOn)
+        valueSlider.setValue(Float(randomValue), animated: animateSwitch.isOn)
+        updateLabel()
+    }
+    
+    func updateLabel() {
+        valueLabel.text = String(format: "%.2f", knob.value)
     }
 
-
 }
-
